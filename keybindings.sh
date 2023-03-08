d1dc733dc899a4478dce744af96028a0c396d572
Russell Cloak <russellcloak@russells-mbp-2.lan>
2023-03-07 19:55:12 -0500

    Created bash file to set windows-style keybindings
    These are the keybindings for home, end, pageup, etc... Just run
    the script and it should work as expected.

---
+++ keybindings.sh
@@ -0,0 +1,26 @@
+DIR=~/Library/KeyBindings
+FILE=$DIR+'/'+DefaultKeyBinding.dict
+
+
+# Make keybindings folder if it doesn't exist
+[ ! -d "$DIR" ] && mkdir $DIR
+
+# Don't recrete the file if it already exists
+[ -f $FILE ] && exit 1 
+
+# Create the file
+touch $FILE
+
+# Add the content
+cat << EOF >> $FILE
+{
+    "\UF729"  = moveToBeginningOfParagraph:; // home
+    "\UF72B"  = moveToEndOfParagraph:; // end
+    "$\UF729" = moveToBeginningOfParagraphAndModifySelection:; // shift-home
+    "$\UF72B" = moveToEndOfParagraphAndModifySelection:; // shift-end
+    "^\UF729" = moveToBeginningOfDocument:; // ctrl-home
+    "^\UF72B" = moveToEndOfDocument:; // ctrl-end
+    "^$\UF729" = moveToBeginningOfDocumentAndModifySelection:; // ctrl-shift-home
+    "^$\UF72B" = moveToEndOfDocumentAndModifySelection:; // ctrl-shift-end
+}
+EOF
